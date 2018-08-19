48f005d5fcdc6c09b5d3fd0c1e5263e875762328
teamjuli0 <juliovaldez@zoho.com>
2018-08-18 18:29:18 -0700

    Got website fully functioning (I think). All I need now is to update all the links in my portfolio.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,37 @@
-$(document).ready(function() {
-    
-    var home = $("#home");
-    var aboutMe = $("#aboutMe");
-    var contact = $("#contact");
-    
-    aboutMe.hide();
-    contact.hide();
+$(document).ready(function () {
 
-    $("#homeButton").on("click", function(){
-        home.show();
-        aboutMe.hide();
-        contact.hide();
+    var homeButton = $("#homeButton");
+    var aboutMeButton = $("#aboutMeButton");
+    var contactButton = $("#contactButton");
+    var uniqueHeight = $(window).height()
+
+    $("#rayRayWebsite").on("click", function () {
+        var productLink = $(this).attr("href", "https://teamjuli0.github.io/SuperDuperAmazingSpectacular/index.html");
+        productLink.attr("target", "_blank");
+        window.open(productLink.attr("href"));
+    });
+
+    $("#triviaGameWebsite").on("click", function () {
+        var productLink = $(this).attr("href", "https://teamjuli0.github.io/TriviaGame/");
+        productLink.attr("target", "_blank");
+        window.open(productLink.attr("href"));
+    });
+
+    $("#3rdWebsite").on("click", function () {
+        var productLink = $(this).attr("href", "https://teamjuli0.github.io/SuperDuperAmazingSpectacular/index.html");
+        productLink.attr("target", "_blank");
+        window.open(productLink.attr("href"));
+    });
+
+    homeButton.on("click", function() {
+        $("html, body").animate({scrollTop:$("#unique").offset().top}, "slow")
     })
 
-    $("#aboutMeButton").on("click", function(){
-        home.hide();
-        aboutMe.show();
-        contact.hide();
+    aboutMeButton.on("click", function() {
+        $("html, body").animate({scrollTop:$(".aboutMe").offset().top}, "slow")
     })
 
-    $("#contactButton").on("click", function(){
-        home.hide();
-        aboutMe.hide();
-        contact.show();
+    contactButton.on("click", function() {
+        $("html, body").animate({scrollTop:$("#contact").offset().top}, "slow")
     })
-
 })
