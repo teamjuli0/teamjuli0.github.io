2bf879baad95ef03dd77f04ed8a4f5a1b6ff1b7c
teamjuli0 <juliovaldez@zoho.com>
2018-08-21 23:57:32 -0700

    After realizing that php does not work on github, i have finally got a working contact form! Finally finished with my mvpgit add -A!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,20 +34,20 @@ $(document).ready(function () {
         $("html, body").animate({ scrollTop: $("#contact").offset().top }, "slow")
     })
 
-    $(".submit").on("click", function () {
-        event.preventDefault();
-        var email = $(".email").val().trim()
-        var name = $(".name").val().trim()
-        var message = $(".message").val().trim()
-        var statusElm = $(".status")
+    // $(".submit").on("click", function () {
+    //     event.preventDefault();
+    //     var email = $(".email").val().trim()
+    //     var name = $(".name").val().trim()
+    //     var message = $(".message").val().trim()
+    //     var statusElm = $(".status")
         
-        setTimeout(function(){statusElm.empty()}, 5000)
+    //     setTimeout(function(){statusElm.empty()}, 5000)
 
-        if (email.length > 5 && email.includes("@") && email.includes(".") && name.length > 2 && message.length > 5) {
-            statusElm.append('<h3 class="uk-logo">Message Is Valid!</h3>')
-        } else {
-            statusElm.append('<h3 class="uk-logo">Please Complete Form Correctly!</h3>')
-        }
-        $("#form")[0].reset();
-    })
+    //     if (email.length > 5 && email.includes("@") && email.includes(".") && name.length > 2 && message.length > 5) {
+    //         statusElm.append('<h3 class="uk-logo">Message Is Valid!</h3>')
+    //     } else {
+    //         statusElm.append('<h3 class="uk-logo">Please Complete Form Correctly!</h3>')
+    //     }
+    //     $("#form")[0].reset();
+    // })
 })
